7983f267c280b3ee912c056b39c8029567e6d285
王钦 <1458192488@qq.com>
2020-02-15 17:45:30 +0800

    文件上传

---
+++ src/main/java/com/swpu/sharebook/service/impl/FileServiceImpl.java
@@ -0,0 +1,20 @@
+package com.swpu.sharebook.service.impl;
+
+import com.swpu.sharebook.service.FileService;
+import com.swpu.sharebook.util.returnvalue.ResponseResult;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileServiceImpl implements FileService {
+
+    @Override
+    public ResponseResult uploadAvatar(MultipartFile file) {
+
+
+
+        
+
+        return null;
+    }
+}
